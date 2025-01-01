4a3a8e92531fccb6850acf1e6f0ef9602ad7775e
Sridhar <09sridhar09@gmail.com>
2025-01-01 19:51:24 +0530

    new

--- day-1-1jan25/src/firstcode.java
+++ day-1-1jan25/src/firstcode.java
@@ -1,5 +1,40 @@
-public class firstcode {
+import java.util.Scanner;
+
+class firstcode {
     public static void main(String[] args) {
-        System.out.println("hi how are you");
+        Scanner sc = new Scanner(System.in);
+
+        //  the size of the array
+      int n = sc.nextInt();
+        int nums[] = new int[n];
+
+        //  the array elements
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        // Finding the count of numbers with even digits
+        int result = findNumbers(nums);
+        System.out.println(result);
+
+        sc.close(); // Closing the scanner to avoid resource leaks
+    }
+
+    static int findNumbers(int[] nums) {
+        int totalcount = 0;
+
+        // Counting digits in each number
+        for (int num : nums) {
+            int noofdigits = 0;
+            while (num != 0) {
+                noofdigits++;
+                num = num / 10;
+            }
+            if (noofdigits % 2 == 0) {
+                totalcount++;
+            }
+        }
+
+        return totalcount;
     }
 }
