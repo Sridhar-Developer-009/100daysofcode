94cd705bff8a48b136a2de2f79381cdd92edae3e
Sridhar <09sridhar09@gmail.com>
2025-01-02 14:18:55 +0530

    pattern8 with spaces and odd number of stars

---
+++ day-2-2jan25/src/pattern9.java
@@ -0,0 +1,30 @@
+/*
+9th question from pattern file
+
+*/
+
+
+import  java.util.*;
+public class pattern9{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pattern(n);
+
+
+    }
+    static void pattern(int n) {
+        for(int row=0;row<n;row++){
+            for(int space=1;space<=row;space++){
+                System.out.print("  ");
+            }
+            for(int star=1;star<=((2*n)-(2*row+1));star++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+}
+
+
