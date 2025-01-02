523d2faa45fe5e43ab6eac8954c911e2f534a0ae
Sridhar <09sridhar09@gmail.com>
2025-01-02 19:53:15 +0530

    pattern9 with spaces and odd number of stars(reversal of8th)

---
+++ day-2-2jan25/src/pattern10.java
@@ -0,0 +1,29 @@
+/*
+10th question from pattern file
+*/
+
+
+import  java.util.*;
+public class pattern10{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pattern(n);
+
+
+    }
+    static void pattern(int n) {
+        for(int row=0;row<n;row++){
+            for(int space=1;space<=n-row-1;space++){
+                System.out.print(" ");
+            }
+            for(int star=0;star<=row;star++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+}
+
+
