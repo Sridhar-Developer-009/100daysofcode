2e35ae098bb513bdaeab201d8e78d72414da898d
Sridhar <09sridhar09@gmail.com>
2025-01-02 20:10:34 +0530

    spaces and stars

--- day-2-2jan25/src/pattern7.java
+++ day-2-2jan25/src/pattern7.java
@@ -16,6 +16,7 @@ public class pattern7 {
     static void pattern(int n) {
         for(int row=1;row<=n;row++){
             for(int space=1;space<=row-1;space++){
+
                 System.out.print("  ");
             }
             for(int star=1;star<=n-row+1;star++){
@@ -26,3 +27,5 @@ public class pattern7 {
         }
     }
 }
+
+
