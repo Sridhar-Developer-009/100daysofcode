2facba43b3e977599b95f9cd15a2ed842fa8c0a6
Sridhar <09sridhar09@gmail.com>
2025-01-03 20:05:19 +0530

    recursion learning

---
+++ day-3-3jan25/src/recursionpractice.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class recursionpractice {
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int ans=fibo(n);
+    System.out.println(ans);
+     }
+static int fibo(int n) {
+// base condition
+    if (n < 2) {
+        return n;
+    }
+    return fibo( n-1) + fibo ( n-2);
+}
+}
+/*
+this wont work for large numbers due to more number of computations.
+ */
