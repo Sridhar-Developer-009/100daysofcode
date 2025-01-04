dd0f46a8b8c4058b558958d2f32bc550a2185eeb
Sridhar <09sridhar09@gmail.com>
2025-01-04 12:45:45 +0530

    pattern11 opposite of pattern10

--- day-4-4jan25/src/pattern11.java
+++ day-4-4jan25/src/pattern11.java
@@ -15,7 +15,7 @@ public class pattern11{
     }
     static void pattern(int n) {
         for(int row=0;row<n;row++){
-            for(int space=0;space<=row;space++){
+            for(int space=0;space<row;space++){
                 System.out.print(" ");
             }
             for(int star=0;star<n-row;star++){
