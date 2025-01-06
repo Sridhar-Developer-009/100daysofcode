dcf229aff62b1045bb929ef3459507075089ec99
Sridhar <09sridhar09@gmail.com>
2025-01-06 20:36:09 +0530

    notes

---
+++ day-5-5jan25/src/pattern16.java
@@ -0,0 +1,19 @@
+/*
+question 15 from patternfile
+
+*/
+
+import java.util.*;
+public class pattern16 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pattern(n);
+    }
+    static void pattern(int n) {
+        for(int row=1;row<=;row++){
+
+            System.out.println();
+        }
+    }
+}
