f08696384ecac66230c267fb6c13f1c0d0fdc643
Sridhar <09sridhar09@gmail.com>
2025-01-11 19:10:01 +0530

    bubblesort easy

--- day-9-9jan25/src/bubblesort.java
+++ day-9-9jan25/src/bubblesort.java
@@ -14,14 +14,21 @@ public class bubblesort {
 
     }
     static void bubble(int n,int[] arr){
+        boolean swapped;
         for(int i=0;i<n;i++){
+            swapped=false;
             for(int j=1;j<n-i;j++){
                 if(arr[j]<arr[j-1]){
                     int temp=arr[j];
                     arr[j]=arr[j-1];
                     arr[j-1]=temp;
+                    swapped=true;
                 }
             }
+            if(!swapped){
+                break;
+            }
         }
+
     }
 }
