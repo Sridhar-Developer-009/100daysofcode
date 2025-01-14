2557b31701342a9f78efc1450e8946a502456d32
Sridhar <09sridhar09@gmail.com>
2025-01-14 19:08:53 +0530

    !

--- day-13-13jan/src/floor.java
+++ day-13-13jan/src/floor.java
@@ -9,9 +9,6 @@ public class floor {
     static int binarysearchh(int arr[],int target){
         int start=0;
         int end=arr.length-1;
-        if(target>arr[arr.length-1]){
-            return -1;
-        }
         while(start<=end){
             int mid=start +(end-start)/2;
             if(target>arr[mid]){
